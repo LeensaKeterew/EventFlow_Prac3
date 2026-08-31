1dbfdc6847a0fdda3e7d20207e7b69c5b57337fd
Joelle-Tsaku <joeltsaku@gmail.com>
2026-08-31 15:31:13 +0200

    EventComponent composite declarations

---
+++ EventGroup.h
@@ -0,0 +1,25 @@
+#ifndef EVENTGROUP_H
+#define EVENTGROUP_H
+
+#include <vector>
+#include "EventComponent.h"
+
+class EventGroup : public EventComponent {
+    private:
+        bool open;
+        std::vector<EventComponent*> children; 
+
+    public: 
+        EventGroup(const std::string& name); 
+        ~EventGroup() override;
+        void open() override;
+        void close() override;
+        void reportStatus() const override;
+        int getCapacity() const override;
+        int getActiveStaffCount() const override; 
+        void add(EventComponent* child); 
+        EventComponent* remove(EventComponent* child); 
+
+};
+
+#endif
