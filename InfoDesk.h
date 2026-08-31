7b2052a2e4a9f05c683bff920ef5cd47a1ac1630
Nokhaya21 <nokhanyafoloti@gmail.com>
2026-08-31 18:26:36 +0200

    InfoDesk Class setup

---
+++ InfoDesk.h
@@ -0,0 +1,19 @@
+#ifndef INFO_DESK_H
+#define INFO_DESK_H
+
+#include "EventUnit.h"
+
+class InfoDesk:public EventUnit{
+    public:
+    InfoDesk(const std::string& name, int capacity);
+    ~InfoDesk() override;
+    void open() override;
+    void close() override;
+    void reportStatud() const override;
+    void update(const Notice& notice) override;
+
+    private:
+    std::string announcement_;
+};
+
+#endif
